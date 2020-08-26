878f6a1dfbabbf19f180741c3ba1f35b93a27762
IncognitusZ <70290523+IncognitusZ@users.noreply.github.com>
2020-08-27 04:31:49 +0800

    Add files via upload

---
+++ Coursera Applied Data Science Capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Applied Data Science Capstone
+
+# This notebook is used for the capstone project
+
+# In[10]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+import numpy as np
+
+
+# In[12]:
+
+
+print("Hello Capstone Project Course!")
+
