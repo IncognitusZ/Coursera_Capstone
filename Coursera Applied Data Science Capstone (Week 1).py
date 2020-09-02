a49e27d7ef2f1a100e30f9fe8d6ff466db5c4ebf
IncognitusZ <70290523+IncognitusZ@users.noreply.github.com>
2020-09-03 03:59:45 +0800

    Update Coursera Applied Data Science Capstone (Week 1).py

--- Coursera Applied Data Science Capstone (Week 1).py
+++ Coursera Applied Data Science Capstone (Week 1).py
@@ -5,19 +5,19 @@
 
 # This notebook is used for the capstone project
 
-# In[10]:
+# In[1]:
 
 
 import pandas as pd
 
 
-# In[11]:
+# In[2]:
 
 
 import numpy as np
 
 
-# In[12]:
+# In[3]:
 
 
 print("Hello Capstone Project Course!")
